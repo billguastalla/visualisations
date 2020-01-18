dc8167355c05211abc0296d99e43e780db6dab7f
Bill Guastalla <billguastalla@gmail.com>
2020-01-18 19:10:25 +0000

    -> Scene has draw() and addModel(), not completed yet, or plugged into project.

--- Projects/Visualisations/IMGUI/Scene.h
+++ Projects/Visualisations/IMGUI/Scene.h
@@ -2,18 +2,65 @@
 #include "Mesh.h"
 #include <map>
 
+
+typedef unsigned int shaderID;
+typedef unsigned int meshID;
+typedef unsigned int modelID;
+
 /* A scene will likely be useful for the tree-field visualisation. */
 class Scene
 {
 public:
+	Scene()
+		:
+		m_lastModel{ 0 },
+		m_lastMesh{0},
+		m_lastShader{0},
+
+		m_meshes{},
+		m_meshIDs{},
+		m_models{},
+		m_shaderIDs{},
+		m_shaders{}
+	{
+
+	}
+
+	void addModel(const glm::mat4& model, std::shared_ptr<Mesh>& mesh, Shader* shader)
+	{
+		/*
+			1. a. get new model id
+				b. add model. 
+			2. a. see if shader already created
+				if so:
+					get shader id
+				else:
+					add shader, with new last shader id
+			3. Do the same for mesh.
+		*/
+		modelID newModelID = ++m_lastModel;
+		//auto existingShader = m_shaders.find(shader);
+		//auto existingModel = m_models.find();
+
+	}
 
 	void draw()
 	{
 		for (auto i = m_models.begin(); i != m_models.end(); ++i)
 		{
-			const unsigned int& k = i->first;
-			auto m = m_meshes.find(k);
-			auto s = m_shaders.find(k);
+			const modelID& moID = i->first;
+			const glm::mat4& model = i->second;
+
+			auto meID = m_meshIDs.find(moID);
+			auto shID = m_shaderIDs.find(moID);
+
+			auto mesh = m_meshes.find(meID->second);
+			auto shader = m_shaders.find(shID->second);
+			if (mesh != m_meshes.end() && shader != m_shaders.end())
+			{
+				shader->second->setMat4("model", model);
+				mesh->second->draw(shader->second);
+			}
 			//m->second.draw(s->second);
 		}
 	}
@@ -25,9 +72,15 @@ private:
 		return result;
 	}
 	/* An index-modelmatrix key-value pair is unique per model.*/
-	std::map<unsigned int, glm::mat4> m_models;
+	std::map<modelID, glm::mat4> m_models;
+	std::map<modelID, shaderID> m_shaderIDs;
+	std::map<modelID, meshID> m_meshIDs;
 
-	/* Shaders (wait do you need a multimap here?)*/
-	std::multimap<unsigned int, std::shared_ptr<Shader>> m_shaders;
-	std::multimap<unsigned int, Mesh> m_meshes;
+	/* Shaders, TODO: shared_ptr<Shader> */
+	std::map<shaderID, Shader*> m_shaders;
+	std::map<meshID, std::shared_ptr<Mesh>> m_meshes;
+
+	modelID m_lastModel;
+	shaderID m_lastShader;
+	meshID m_lastMesh;
 };
