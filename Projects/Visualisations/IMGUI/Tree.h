e40750bc94f754134c1f1f2abab587b383266565
Bill Guastalla <billguastalla@gmail.com>
2020-01-25 17:19:47 +0000

    -> Thinking about design of tree, need a connector to split cylinders

--- Projects/Visualisations/IMGUI/Tree.h
+++ Projects/Visualisations/IMGUI/Tree.h
@@ -3,6 +3,22 @@
 
 class Node;
 
+/*
+	-> Most poignant question here is how do we wind a tree with a continuous mesh?
+		\ /
+		 |
+		-> Two holes should appear in the splitting of a branch.
+		-> We need to change the way that indices are written, at the point of splitting.
+			-> There appears to need to be a connector model, which has one hole for the base,
+				and N holes for the branches. (upside down pants)
+				-> The connector needs to know the angles phi and theta of the branches.
+					(possibly also their radius)
+					-> there appear to be semi-circles joining opposite sides of the base
+						circle, which split the branches.
+					-> We want to control the resolution of the connector.
+					-> We want to pass an ellipsoid shape into a branch, so that the
+						branch mesh can smoothly wind the ellipsoid into a cylinder.
+*/
 class Tree
 {
 
